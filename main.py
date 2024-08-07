f93c02657ac07fbfe2213cdd13afdd034648bc9d
CONDRAD26 <condradotim199@gmail.com>
2024-08-07 13:31:04 +0300

    conie 2..

--- main.py
+++ main.py
@@ -9,15 +9,15 @@ def model_prediction(test_image):
     image = tf.keras.preprocessing.image.load_img(test_image,target_size=(128,128))
     input_arr = tf.keras.preprocessing.image.img_to_array(image)
     input_arr = np.array([input_arr]) 
-    prediction =model.predict(input_arr)
-    result_index =np.argmax(prediction)
+    prediction = model.predict(input_arr)
+    result_index = np.argmax(prediction)
     return result_index
 
 # Sidebar 
 st.sidebar.markdown(
         """
         <div style="text-align: ;">
-            <h1 style="color: #ff6347;">Huevos</h1>
+            <h1 style="color: #ff6347;">Quevos</h1>
         </div>
         """,
         unsafe_allow_html=True
@@ -29,10 +29,10 @@ app_mode = st.sidebar.selectbox("Select page",["Home", "About", "Egg Recognition
 # Home page
 if(app_mode == "Home"):
     st.header("Egg Class Recognition System")
-    image_path ="Handle with Care.png" 
-    st.image(image_path,use_column_width=True)
+    image_path ="image.jpeg"
+    st.image(image_path, width=450) 
     st.markdown("""
- ## Welcome to Our Egg Class Detection App! 🥚
+## Welcome to Our Egg Class Detection App.
 
 ### About Us
 
@@ -59,43 +59,37 @@ elif(app_mode =="About"):
     st.header("About ")
     st.markdown("""
     ### About Dataset
+    The dataset was collected on a farm and comprises images of eggs, categorized into four distinct classes: Class AA, Class A, Class B, and Stale. This dataset is crucial for developing a machine learning model aimed at classifying eggs based on their quality. The images were captured in a controlled environment to ensure consistency in lighting and background, providing a reliable basis for training and testing the model. Each class represents a specific quality of eggs, with clear visual distinctions that facilitate accurate classification.
     """) 
-               
+            
 #Prediction Page 
-elif(app_mode == "Egg Recognition"): 
+elif(app_mode == "Egg Recognition"):
     st.header("Egg Recognition")
-    test_image = st.file_uploader("Choose an Image") 
+    test_image = st.file_uploader("Choose an Image")
     if(st.button("Show Image")):
         st.image(test_image,use_column_width=True)
         #Predict Button
     if(st.button("Predict")):
         with st.spinner("Please Wait....."):
             st.snow()
-            st.write("Our prediction") 
+            st.write("Our prediction")
             result_index = model_prediction(test_image)
         #Define Classname
-            class_name = ['Egg_Class B', 'Egg_Fresh Class AA', 'Egg_Fresh Class B', 'Egg_stale']   
-            st.success("model is Predicting it's a {}".format(class_name[result_index]))   
+            class_name = ['Egg_Class B', 'Egg_Fresh Class AA', 'Egg_Fresh Class B', 'Egg_stale']
+            st.success("model is Predicting it's a {}".format(class_name[result_index]))
         
 #Egg Recognition2
-elif(app_mode == "Egg Recognition(take a photo)"): 
+elif(app_mode == "Egg Recognition(take a photo)"):
     st.header("Egg Recognition(take a photo)")
-    test_image = st.camera_input("Take an Image") 
-    if st.button("Save Image"):
-        # Define the filename to save the image
-        filename = "saved_image.jpg"  # You can customize the filename as needed
-        
-        # Save the image
-        test.save(filename)
-        
-        # Provide feedback to the user
-        st.success(f"Image saved as {filename}")
-    #Predict Button
+    test_image = st.camera_input("Take an Image")
+    if(st.button("Show Image")):
+        st.image(test_image,use_column_width=True)
+        #Predict Button
     if(st.button("Predict")):
         with st.spinner("Please Wait....."):
             st.snow()
-            st.write("Our prediction") 
-            result_index = model_prediction(test_image) 
+            st.write("Our prediction")
+            result_index = model_prediction(test_image)
         #Define Classname
-            class_name = ['Egg_Class B', 'Egg_Fresh Class AA', 'Egg_Fresh Class B', 'Egg_stale']   
+            class_name = ['Egg_Class B', 'Egg_Fresh Class AA', 'Egg_Fresh Class B', 'Egg_stale']
             st.success("model is Predicting it's a {}".format(class_name[result_index]))
